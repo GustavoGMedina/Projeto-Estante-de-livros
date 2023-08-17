f775f61f070dbbb9cb9693ede8e2ab2a6804e485
GustavoGMedina <139518683+GustavoGMedina@users.noreply.github.com>
2023-08-16 22:03:00 -0300

    Add files via upload

---
+++ locadora.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Estante de Livros</title>
+  <style>
+    table {
+      border-collapse: collapse;
+      width: 100%;
+    }
+    th, td {
+      text-align: left;
+      padding: 8px;
+    }
+    th {
+      background-color: #f2f2f2;
+    }
+    .book-cover {
+      max-width: 100px;
+    }
+  </style>
+</head>
+<body>
+  <h1>Estante de Livros</h1>
+  
+  <table>
+    <thead>
+      <tr>
+        <th>Título</th>
+        <th>Autor</th>
+        <th>Gênero</th>
+        <th>Número de Páginas</th>
+        <th>Editora</th>
+        <th>Sinopse</th>
+        <th>Capa do Livro</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>O Livro dos Espelhos</td>
+        <td>E.O. Chirovici</td>
+        <td>Thriller</td>
+        <td>261</td> 
+        <td>Grupo Editorial Record</td> 
+        <td>Trata-se de um livro de memórias sobre o tempo de estudante de inglês de Flyn na Universidade de Princenton, em 1987, onde documentou sua relação com o famoso professor de psicologia Joseph Wieder e sua protegida e amante, Laura Baines. Uma noite antes do Natal de 1987, Wieder foi brutalmente assassinado em sua casa.</td>
+        <td><img src="C:\wamp64\www\curso2\img\livro dos espelhos.jpg" alt="Capa do Livro" class="book-cover"></td>
+      </tr>
+      <tr>
+        <td>O Grande Gatsby</td>
+        <td>F. Scott Fitzgerald</td>
+        <td>Romance</td>
+        <td>221</td>
+        <td>Charles Scribner's Sons</td> 
+        <td>Na primavera de 1922, Nick Carraway chega a Nova York e vira vizinho do misterioso e festeiro milionário Jay Gatsby quando vai viver do outro lado da baía com sua prima Daisy e seu marido mulherengo Tom Buchanan. Assim, Nick é atraído para o mundo cativante dos ricos, suas ilusões, amores e fraudes. Ao testemunhar fatos dentro e fora do mundo em que habita, Nick escreve um conto de amor impossível, sonhos e tragédias que espelham conflitos em tempos modernos.</td>
+        <td><img src="C:\wamp64\www\curso2\img\O Grande Gatsby.jpg" alt="Capa do Livro" class="book-cover"></td>
+      </tr>
+    </tbody>
+  </table>
+  <script>
+  </script>
+</body>
+</html>
